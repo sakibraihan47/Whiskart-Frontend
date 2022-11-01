b84f0847825966b4af83d2a979c1246c1e490531
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-11-01 13:18:23 +0600

    ui: #3 Navbar Updated

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,9 +1,24 @@
-const About = () => {
+const AboutUs = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <a
+          href="#"
+          class="block p-6 max-w-full bg-white  border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+            about us.
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            voluptatem hic asperiores dignissimos ut voluptatibus inventore
+            consequatur quos, odit perferendis pariatur laboriosam nihil aliquam
+            distinctio praesentium, ducimus fugiat veritatis. Sequi.
+          </p>
+        </a>
+      </div>
     </>
   );
 };
 
-export default About;
+export default AboutUs;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import { NavbarSignIn } from "../components/NavbarSignIn";
+import { NavbarArtist } from "../components/NavbarArtist";
+import AboutUs from "../components/AboutUs";
 
 export default function Home() {
   return (
     <>
-      <NavbarSignIn />
+      <NavbarArtist />
     </>
   );
 }
