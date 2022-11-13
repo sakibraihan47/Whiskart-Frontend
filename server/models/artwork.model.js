773c40dd1174a69765dcc73b1786bdb02d281b7a
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-11-13 21:45:23 +0600

    #10  build: added timestamp

--- server/models/artwork.model.js
+++ server/models/artwork.model.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const artworkSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  artist: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  qty: { type: String, required: true },
-  des: { type: String, required: true },
-  canvas: { type: String, required: true },
-  color: { type: String, required: true },
-  price: { type: String, required: true },
-  genre: { type: String, required: true },
-  img: { type: String, required: true },
-});
+const artworkSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    artist: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    qty: { type: String, required: true },
+    des: { type: String, required: true },
+    canvas: { type: String, required: true },
+    color: { type: String, required: true },
+    price: { type: String, required: true },
+    genre: { type: String, required: true },
+    img: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Artwork = mongoose.model("Artworks", artworkSchema);
 module.exports = Artwork;
