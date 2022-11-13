5848b22df4935a2ec650aa21209fb1f612106061
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-11-13 21:44:56 +0600

    #4 style: css changes

--- pages/gallery.js
+++ pages/gallery.js
@@ -7,62 +7,64 @@ const Gallery = ({ artworks }) => {
   return (
     <>
       {artworks.map((artwork) => (
-        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" class="py-3 px-6">
-                  Artwork
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Description
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Color
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Genre
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Canvas
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Price
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  Posted On
-                </th>
-                <th scope="col" class="py-3 px-6">
-                  <span class="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {artwork.name}
-                </th>
-                <td class="py-4 px-6"> {artwork.des}</td>
-                <td class="py-4 px-6"> {artwork.color}</td>
-                <td class="py-4 px-6"> {artwork.genre}</td>
-                <td class="py-4 px-6"> {artwork.canvas}</td>
-                <td class="py-4 px-6"> {artwork.price}</td>
-                <td class="py-4 px-6">h {artwork.postedon}</td>
-
-                <td class="py-4 px-6 text-right">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-gray-800 to-purple-900">
+          <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" class="py-3 px-6">
+                    Artwork
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Description
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Color
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Genre
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Canvas
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Price
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Posted On
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                    {artwork.name}
+                  </th>
+                  <td class="py-4 px-6"> {artwork.des}</td>
+                  <td class="py-4 px-6"> {artwork.color}</td>
+                  <td class="py-4 px-6"> {artwork.genre}</td>
+                  <td class="py-4 px-6"> {artwork.canvas}</td>
+                  <td class="py-4 px-6"> {artwork.price}</td>
+                  <td class="py-4 px-6">{artwork.createdAt}</td>
+
+                  <td class="py-4 px-6 text-right">
+                    <a
+                      href="#"
+                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       ))}
     </>
