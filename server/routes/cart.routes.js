77ab63ecbf80d634771472dc63308301f85b9315
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-12-09 00:17:32 +0600

    #14 build: cart item duplicate check route creater

--- server/routes/cart.routes.js
+++ server/routes/cart.routes.js
@@ -4,9 +4,13 @@ const { addCart } = require("../controllers/cart.controller.js");
 const { getCart } = require("../controllers/cart.controller.js");
 const { deleteItemCart } = require("../controllers/cart.controller.js");
 const { getCartItem } = require("../controllers/cart.controller.js");
+const { checkCart } = require("../controllers/cart.controller.js");
 const auth = require("../middleware/auth");
 
 router.post("/mycart", auth, addCart);
+module.exports = router;
+
+router.post("/checkcart", auth, checkCart);
 module.exports = router;
 
 router.get("/getmycart/:userId", auth, getCart);
