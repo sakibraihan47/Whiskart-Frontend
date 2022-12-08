5e5eb74cc247a7b6c25790e4d7d96b6d6f49dee0
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-12-09 00:23:01 +0600

    #11 ui: Created Recent Page

--- pages/recent.js
+++ pages/recent.js
@@ -6,4 +6,27 @@ const recent = () => {
   );
 };
 
+export const getServerSideProps = async (context) => {
+  const token = context.req.cookies.token;
+  if (!token) {
+    return { redirect: { permanent: true, destination: "/" }, props: {} };
+  }
+  const { params } = context;
+  const { id } = params;
+
+  let res = await fetch(`http://localhost:3002/getthisartwork/${id}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  res = await res.json();
+  return {
+    props: { artwork: res.artworks, token: token },
+  };
+};
+
 export default recent;
