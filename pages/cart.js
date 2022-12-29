d068f9c9d735b1804ba946ed20a4c6eadbce0557
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2022-12-29 17:23:10 +0600

    ui: #16 Created Like button and added functions

--- pages/cart.js
+++ pages/cart.js
@@ -24,8 +24,8 @@ const cart = ({ cart }) => {
 
   
 
-  // const { updateCartCount } = useContext(AuthContext);
-  const { updateCartCount } = cart.length
+  const { updateCartCount } = useContext(AuthContext);
+
 
 
   
--- pages/home/[id].js
+++ pages/home/[id].js
@@ -33,9 +33,11 @@ console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ recommend", recommend)
   const router = useRouter();
   // const [recArt, setRecArt] = useState([]);
   const [chkCart, setCheckCart] = useState([]);
+  const [chkLike, setCheckLike] = useState()
+  const [count, setCount] = useState(artwork.like)
   const [cartContent, setCartContent] = useState([]);
-  console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ chkCart", chkCart);
-
+  console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ chkCart", count);
+  
   // useEffect(() => {
   //   // recommendTo();
   //   // checkCart();
@@ -52,6 +54,9 @@ console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ recommend", recommend)
     console.log("🚀 ~ file: [id].js:49 ~ artworkInfo ~ artwork", artwork)
 
   }, [cartContent]);
+
+ 
+  
   
   // const recommendTo = async () => {
   //   try {
@@ -74,6 +79,32 @@ console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ recommend", recommend)
   //     console.log(error);
   //   }
   // };
+
+
+
+const like = async() =>{
+  
+  let counter = count + 1
+  setCount(counter)
+  console.log("🚀 ~ file: [id].js:81 ~ like ~ counter", counter)
+    console.log("🚀 ~ file: [id].js:84 ~ like ~ artwork.id", artwork._id)
+  const res = await fetch("http://localhost:3002/like/" + artwork._id, {
+    
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({like: counter}),
+    
+  });
+
+  const data = await res.json();
+ 
+}
+
 
   const addCart = async () => {
    
@@ -263,11 +294,25 @@ console.log("🚀 ~ file: [id].js:24 ~ artworkInfo ~ recommend", recommend)
                       Out of Stock!
                     </a>
                   )}
+                     <div class="flex flex-col space-x-1">
+                    
+                    <div
+                      class="bg-pink-600 text-white cursor-pointer px-3 text-center justify-center items-center py-1 rounded-xl flex space-x-2 flex-row"
+                      onClick={like}>
+                  
+                      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="text-xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M885.9 533.7c16.8-22.2 26.1-49.4 26.1-77.7 0-44.9-25.1-87.4-65.5-111.1a67.67 67.67 0 0 0-34.3-9.3H572.4l6-122.9c1.4-29.7-9.1-57.9-29.5-79.4A106.62 106.62 0 0 0 471 99.9c-52 0-98 35-111.8 85.1l-85.9 311H144c-17.7 0-32 14.3-32 32v364c0 17.7 14.3 32 32 32h601.3c9.2 0 18.2-1.8 26.5-5.4 47.6-20.3 78.3-66.8 78.3-118.4 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7-.2-12.6-2-25.1-5.6-37.1zM184 852V568h81v284h-81zm636.4-353l-21.9 19 13.9 25.4a56.2 56.2 0 0 1 6.9 27.3c0 16.5-7.2 32.2-19.6 43l-21.9 19 13.9 25.4a56.2 56.2 0 0 1 6.9 27.3c0 16.5-7.2 32.2-19.6 43l-21.9 19 13.9 25.4a56.2 56.2 0 0 1 6.9 27.3c0 22.4-13.2 42.6-33.6 51.8H329V564.8l99.5-360.5a44.1 44.1 0 0 1 42.2-32.3c7.6 0 15.1 2.2 21.1 6.7 9.9 7.4 15.2 18.6 14.6 30.5l-9.6 198.4h314.4C829 418.5 840 436.9 840 456c0 16.5-7.2 32.1-19.6 43z"></path></svg>
+
+
+
+<span>{count}</span>
+                    </div>
+                  </div>
                 </div>
                
               </div>
             </div>
           </div>
+          
           {recommend.length >0 && (
           <div className="flex ml-4 w-72 h-screen justify-center items-center">
             {/* <a href="#">
